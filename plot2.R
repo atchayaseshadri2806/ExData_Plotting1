991028ed359463224b1c52444c0135c95265da19
Atchaya Seshadri <59754230+atchayaseshadri2806@users.noreply.github.com>
2020-06-24 16:52:23 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,5 @@
+##Plot 2
+plot(data$Global_active_power~data$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+##Saving to file:
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
